31bb3ab3092cd9e9f3adae705114dfcde5ed9e08
JIMPIN <jmpinard@clinellc.com>
2019-09-11 12:40:04 -0400

    -Dump 9-11 Rev2

--- force-app/main/default/lwc/workOrderDataTable/workOrderDataTable.js
+++ force-app/main/default/lwc/workOrderDataTable/workOrderDataTable.js
@@ -3,16 +3,16 @@ import getRecords from '@salesforce/apex/WorkOrderDataTableController.getRecords
 import { getRecord } from 'lightning/uiRecordApi';
 
 const columns = [
-    { label: 'Line Item #', fieldName: 'LineItemNumber', type: 'number' },
+    { label: 'Line Item #', fieldName: 'LineItemNumber', type: 'number', cellAttributes: { alignment: 'center' }},
     { label: 'Description', fieldName: 'Description', type: 'Text' },
-    { label: 'Item Type', fieldName: 'Item_Type__c', type: 'Text' },
-    { label: 'Quantity', fieldName: 'Quantity', type: 'number' },
-    { label: 'UOM', fieldName: 'Product_Qty_Unit_of_Measure_Formula__c', type: 'Text' },
-    { label: 'Product Cost', fieldName: 'Product_Cost__c', type: 'currency' },
-    { label: 'List Price', fieldName: 'ListPrice', type: 'currency' },
-    { label: 'Overridden Price', fieldName: 'UnitPrice', type: 'currency' },
-    { label: 'Final Unit Price', fieldName: 'UnitPrice_1__c', type: 'currency' },
-    { label: 'Extended Subtotal', fieldName: 'Total_Price_Commercial__c', type: 'currency' },
+    { label: 'Item Type', fieldName: 'Item_Type__c', type: 'Text', cellAttributes: { alignment: 'center' }},
+    { label: 'Quantity', fieldName: 'Quantity', type: 'number', cellAttributes: { alignment: 'center' }},
+    { label: 'UOM', fieldName: 'Product_Qty_Unit_of_Measure_Formula__c', type: 'Text', cellAttributes: { alignment: 'center' }},
+    { label: 'Product Cost', fieldName: 'Product_Cost__c', type: 'currency', cellAttributes: { alignment: 'center' }},
+    { label: 'Markup %', fieldName: 'Markup__c', type: 'percent', cellAttributes: { alignment: 'center' }},
+    { label: 'List Price', fieldName: 'ListPrice', type: 'currency', cellAttributes: { alignment: 'center' }},
+    { label: 'Final Price', fieldName: 'UnitPrice', type: 'currency', cellAttributes: { alignment: 'center' }},
+    { label: 'Extended', fieldName: 'Total_Price_Commercial__c', type: 'currency', cellAttributes: { alignment: 'center' }},
 ];
 
 export default class WODatatableBasic extends LightningElement {
